34c6739d949e87a6d7da989f7e3ad6e760d956e4
Rohit Ramesh <rkr@berkeley.edu>
2019-04-30 17:12:50 -0700

    Fleshed out a ton of instances.

--- src/Ivy/Scratchpad.hs
+++ src/Ivy/Scratchpad.hs
@@ -11,8 +11,8 @@ Portability : POSIX
 module Ivy.Scratchpad where
 
 import Ivy.Prelude
-
-import Data.Coerce
+import Data.Monoid
+import qualified GHC.Base as Base (mzero, mplus)
 
 class (Monad m) => MonadLat m where
 
@@ -30,66 +30,92 @@ class (Monad m) => MonadLat m where
   default bot :: (MonadPlus m) => m a
   bot = empty
 
+data Lat e a where
+  Top :: e -> Lat e a
+  Val :: a -> Lat e a
+  Bot :: Lat e a
+
+deriving instance Functor (Lat e)
+
+instance Semigroup e => Applicative (Lat e) where
+  pure = Val
+
+  Top e <*> Top e' = Top $ e <> e'
+  Top e <*> _      = Top e
+  _     <*> Top e  = Top e
+  Val f <*> Val a  = Val $ f a
+  Bot   <*> _      = Bot
+  _     <*> Bot    = Bot
+
+instance Semigroup e => Alternative (Lat e) where
+  empty = Bot
+  Bot   <|> a      = a
+  a     <|> Bot    = a
+  Val a <|> _      = Val a
+  _     <|> Val a  = Val a
+  Top e <|> Top e' = Top $ e <> e'
+
+instance Semigroup e => Monad (Lat e) where
+  Bot   >>= _ = Bot
+  Val a >>= f = f a
+  Top e >>= _ = Top e
+
+instance Semigroup e => MonadError e (Lat e) where
+  throwError = Top
+  catchError (Top e) f = f e
+  catchError a       _ = a
+
+instance Semigroup e => MonadPlus (Lat e) where
+  mzero = Bot
+  mplus = (<|>)
+
 data LTerm l k where
   T :: l (LTerm l k) -> LTerm l k
   V :: k -> LTerm l k
 
+deriving instance (Functor l) => Functor (LTerm l)
+
 class POrd l where
 
-  lessThanOrEq :: (MonadLat m) => l -> l -> m Bool
+  lessThanOrEq    :: l -> l -> Bool
+  lessThan        :: l -> l -> Bool
+  greaterThanOrEq :: l -> l -> Bool
+  greaterThan     :: l -> l -> Bool
+  equalTo         :: l -> l -> Bool
+  notEqualTo      :: l -> l -> Bool
 
-  lessThan :: (MonadLat m) => l -> l -> m Bool
-
-  greaterThanOrEq :: (MonadLat m) => l -> l -> m Bool
-
-  greaterThan :: (MonadLat m) => l -> l -> m Bool
-
-  equalTo :: (MonadLat m) => l -> l -> m Bool
-
-  notEqualTo :: (MonadLat m) => l -> l -> m Bool
-
-class Lattice l where
-
+class (POrd l) => Lattice l where
   latBottom :: l
-
   latJoin :: (MonadLat m) => l -> l -> m l
-
   latMeet :: (MonadLat m) => l -> l -> m l
 
-class POrd1 l where
+class (Functor l) => POrdF l where
 
-  liftLessThanOrEq :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
+  liftLessThanOrEq    :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
+  liftLessThan        :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
+  liftGreaterThanOrEq :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
+  liftGreaterThan     :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
+  liftEqualTo         :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
+  liftNotEqualTo      :: (Monad m) => (p -> p -> m Bool) -> l p -> l p -> m Bool
 
-  liftLessThan :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
-
-  liftGreaterThanOrEq :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
-
-  liftGreaterThan :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
-
-  liftEqualTo :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
-
-  liftNotEqualTo :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m Bool
-
-class (Functor l) => LatticeF l where
-
+class (POrdF l) => LatticeF l where
   liftLatBottom :: p -> l p
-
-  liftLatJoin :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m (l p)
-
-  liftLatMeet :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m (l p)
+  liftLatJoin   :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m (l p)
+  liftLatMeet   :: (MonadLat m) => (p -> p -> m p) -> l p -> l p -> m (l p)
 
 {-
 class (Functor (l k)) => LatticeKF k l where
 
-  liftLatBottomWithKey :: p -> l k p
-
-  liftLatJoinWithKey :: (MonadLat m, Lattice p) => (p -> p -> m p) -> l k p -> l k p -> m (l k p)
-
-  liftLatMeetWithKey :: (MonadLat m, Lattice p) => (p -> p -> m p) -> l k p -> l k p -> m (l k p)
+  liftLatBottomWithKey :: p -> l k
+  liftLatJoinWithKey :: (MonadLat m) => (p -> p -> m p) -> l k p -> l k p -> m (l k p)
+  liftLatMeetWithKey :: (MonadLat m) => (p -> p -> m p) -> l k p -> l k p -> m (l k p)
 -}
 
 infixr 0 :-^
 
+-- | This is effectively a constrained category of upwards closed functions.
+--   In an ideal world we could perform all of our operations using this category
+--   but in practice that's inconvenient and so we
 data a :-^ b where
 
   UCFInt ::(Lattice a, Lattice b) =>
@@ -99,13 +125,14 @@ data a :-^ b where
 
 getUCF :: (Lattice a, Lattice b) => (a :-^ b) -> (forall m. MonadLat m => a -> m b)
 getUCF (UCFInt a) = a
-getUCF UCFId = pure
+getUCF UCFId      = pure
 
 instance Category (:-^) where
+
   id = UCFId
 
-  UCFId . a = a
-  a . UCFId = a
+  UCFId      . a          = a
+  a          . UCFId      = a
   (UCFInt a) . (UCFInt b) = UCFInt (b >=> a)
 
 pattern UCF :: (Lattice a, Lattice b) => (forall m. MonadLat m => a -> m b) -> a :-^ b
@@ -128,36 +155,85 @@ class (Monad m) => MonadLatticeMap m where
   subsumes :: (Lattice l) => Key m l -> Key m l -> m Bool
 -}
 
-class (Monad m) => MonadLatticeMapF m where
+class (Monad m, LatticeF l) => MonadLatMapF m l where
 
   data KeyF m (l :: * -> *) :: *
 
-  putLatF  :: (LatticeF l) => LTerm l (KeyF m l) -> m (KeyF m l)
+  putLatF  :: LTerm l (KeyF m l) -> m (KeyF m l)
 
-  bindLatF  :: (LatticeF l) => KeyF m l -> LTerm l (KeyF m l) -> m (KeyF m l)
+  bindLatF  :: KeyF m l -> LTerm l (KeyF m l) -> m (KeyF m l)
 
-  getLatF   :: (LatticeF l) => KeyF m l -> m (LTerm l (KeyF m l))
+  getLatF   :: KeyF m l -> m (LTerm l (KeyF m l))
 
-  equalsF   :: (LatticeF l) => KeyF m l -> KeyF m l -> m (KeyF m l)
+  equalsF   :: KeyF m l -> KeyF m l -> m (KeyF m l)
 
-  subsumesF :: (LatticeF l) => KeyF m l -> KeyF m l -> m Bool
+  subsumesF ::  KeyF m l -> KeyF m l -> m Bool
 
--- | This is a list of changes to the map that we might be able to apply if
---   neccesary.
-data Edit k l
-  = Bind (k l) (LTerm l (k l))
-  | Equals (k l) (k l)
-  | Subsumes (k l) (k l)
+-- | An edit captures a single concrete change we could make to our
+--   lattice map.
+--
+--   TODO :: Stuff like adding new terms in our parent language when we implement
+--          them.
+data Edit m k where
 
--- | As we execute an op over our system, we should be producing transactions as an
---   intermediate state, which allows us to chop things up as neccesary
-data Trans k a where
+  Bind :: (MonadLatMapF m l) => k -> LTerm l k -> Edit m k
 
-  Parallel:: [Trans k a] -> Trans k a
-  Watch :: k a -> (k a -> a -> Trans k b) -> Trans k b
-  Run :: [Edit k l] -> Trans k () -> Trans k ()
-  Done :: Trans k ()
-  Hold :: Trans k ()
+  Equals :: k -> k -> Edit m k
+
+  Subsumes :: k -> k -> Edit m k
+
+deriving instance Functor (Edit m)
+
+
+-- | A Transaction is a suspended computation over the map of terms. When
+--   it suspends it could be:
+--
+--     - A set of hooks which can generate a bunch of transactions when a
+--       key is modified.
+--
+--     - A set of concrete edits to the LatMap we can (presumably) analyse as
+--       needed.
+--
+--     - A signal that this specific transaction is finished, and can be
+--       discarded when we finish,
+data Transaction m k where
+
+  -- | This transaction will add hooks that trigger when specific lattice
+  --   elements are updated.
+  Watch :: HashMap k (k -> m (Transaction m k)) -> Transaction m k
+
+  -- | This transaction represents a series of concrete operations that
+  --   we can perform on a set of lattice elements, and the transaction that
+  --   happens when we
+  Run :: [Edit m k] -> Transaction m k -> Transaction m k
+
+  -- | This is a transaction which is complete. No additional executions are
+  --   necessary and the transaction itself can be discarded.
+  Done :: Transaction m k
+
+instance (Eq k, Hashable k, Applicative m) => Semigroup (Transaction m k) where
+
+  -- We use the Semigroup instance of `Ap` (in Data.Monoid) to allow the
+  -- semigroup instance of HashMap to work over the transactions we return.
+  (Watch m) <> (Watch m') = Watch . unAp $ wrapAp m <> wrapAp m'
+    where
+      wrapAp :: HashMap k (k -> m r) -> HashMap k (k -> Ap m r)
+      wrapAp = map (map Ap)
+
+      unAp :: HashMap k (k -> Ap m r) -> HashMap k (k -> m r)
+      unAp = map (map getAp)
+
+  -- Edits should be pretty declarative
+  (Run e m) <> (Run e' m') = Run (e <> e') (m <> m')
+
+  Run e m <> a = Run e (m <> a)
+  a <> Run e m = Run e (m <> a)
+
+  Done <> a = a
+  a <> Done = a
+
+instance (Eq k, Hashable k, Applicative m) => Monoid (Transaction m k) where
+  mempty = Done
 
 {-
 -- | Do we ever need to allow the type parameter to be different here?
@@ -180,27 +256,24 @@ class (Monad m) => MonadLatticeMapKF m where
     => KeyKF m l -> KeyKF m l -> m Bool
 -}
 
-{-
-uAnd :: MonadLatticeProp m => Lat Bool -> Lat Bool -> Lat Bool
-uAnd a b = fall a <|> fall b <|> base
+-- | Sadly, without some way to introspect whether a term is forced, we can't
+--   have the nicer interface which allows us to write more or less pure
+--   functions and have their fall-through properties inferred.
+--   instead we use the lesser of two evils and work with the lattice elements
+--   more directly.
+uAnd :: (Alternative m, MonadLat m) => m Bool -> m Bool -> m Bool
+uAnd a b = (a >>= fall) <|> (b >>= fall) <|> ((&&) <$> a <*> b)
 
   where
 
-    fall :: m Bool -> m Bool
-    fall = undefined
+    fall False = val False
+    fall True  = bot
 
-    base :: m Bool
-    base = (&&) <$> a <$> b
 
-uOr :: MonadLatticeProp m => Lat Bool -> Lat Bool -> Lat Bool
-uOr a b = fall a <|> fall b <|> base
+uOr :: (Alternative m, MonadLat m) => m Bool -> m Bool -> m Bool
+uOr a b = (a >>= fall) <|> (b >>= fall) <|> ((&&) <$> a <*> b)
 
   where
 
-    fall :: m Bool -> m Bool
-    fall b = undefined
-
-    base :: m Bool
-    base = (||) <$> a <$> b
-
--}
+    fall True  = val True
+    fall False = bot
