1de7e4f63796777b073ce2856eaa838042e6b696
Rohit Ramesh <rkr@berkeley.edu>
2019-07-04 16:07:12 -0700

    An update oriented interface for UnificationMap

--- src/Ivy/Scratchpad.hs
+++ src/Ivy/Scratchpad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-|
 Module      : Ivy.Scratchpad
 Description : Random scratch work goes here until it's moved
@@ -11,10 +12,75 @@ Portability : POSIX
 module Ivy.Scratchpad where
 
 import Ivy.Prelude
-import Control.Monad.Lat.Class
+
+class Generator v g | g -> v where
+  get   :: g -> (v , g)
+  split :: g -> (g , g)
+
+type Ref v = (Eq v, Hashable v)
+
+data TV t v = T (t (TV t v)) | V v
+
+class (Semigroup e, Traversable t, Eq1 t, Hashable1 t)
+                => Unifiable e (t :: Type -> Type) where
+  unifyTerm :: (Ref v, UnificationMap e m t)
+            => t v -> t v -> Either e (t (Either (v,v) v))
+
+-- | This class presents a map of variables to (recursive) terms in a
+--   continuation passing style.
+class (Unifiable e t)
+        => UnificationMap e (m :: Type) (t :: Type -> Type) | m -> t where
+
+  -- | This is an update that tells us how to modify a unification map.
+  type Update m v t :: Type -> Type
+
+  type Frozen m v t :: Type -> Type
+
+  -- | Pass in a continuation that uses a new variable to get a map update
+  --   and get the corresponding update.
+  freeVar  :: (Ref v) => Update m v t v
+
+  -- | Binds a term to a variable
+  bindVar  :: (Ref v) => v -> TV t v -> Update m v t ()
+
+  -- | The first variable is <= the second
+  subsumes :: (Ref v) => v -> v -> Update m v t (Maybe e)
+
+  -- | Given two updates, this produces a parallel merge of those two updates.
+  --   really, it's the implementation of this that determines the degree of
+  --   parallelism that's used as things update and actions are performed.
+  merge    :: (Ref v) => Update m v t a -> Update m v t b -> Update m v t (a,b)
+
+  -- | If you give it a variable whose term you want to get, and what you
+  --   intend to do with that term, then this returns the final update
+  getTerm  :: (Ref v) => v -> Update m v t (Either e (t v))
+
+  -- | Given a generator and an update action, this will generate the
+  --   concrete final mapping that contains all the (fully cyclical)
+  --   post unification terms.
+  build :: (Ref v, Generator v g) => g -> Update m v t b -> Either e (b, m)
+
+  -- | Modify an error, usually used to add context to a low level error.
+  modifyError :: (e -> e) -> Update m v t a -> Update m v t a
 
 
--- Want to detect undermatch vs overmatch
--- I.e. if e expect to see a (a :+ b) and get a vert, then it's an undermatch
---      if I expect to see a (a :+ b) and get a (a :- b) it's an overmatch and
---      can be treated as a permanent failure.
+-- | If you have an expression, generate a new variable
+newVar :: (Monad (Update m v t), UnificationMap e m t) => TV t v -> Update m v t v
+newVar = __TODO
+
+unify :: forall e m t v. (UnificationMap e m t, Ref v, Monad (Update m v t), Semigroup e)
+      => v -> v -> Update m v t (Either e v)
+unify a b = do
+  eta <- getTerm @e @m a
+  etb <- getTerm @e @m b
+  case (eta,etb) of
+    (Left e, Left e') -> pure . Left $ e <> e'
+    (Left e, _) -> pure . Left $ e
+    (_, Left e) -> pure . Left $ e
+    (ta, tb) -> __TODO
+    -- Try unifying the term, w/in a wrapper that assumes a and b are merged.
+    -- if you get an error out then return that.
+    -- otherwise, traverse through the term replacing pairs and terms
+    -- with `Either e a`
+    --   if there are 1 or more errors, `<>` them and return that
+    --   if not then bind the result term to a and return a
