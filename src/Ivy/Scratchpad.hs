95dafed497e4f126b1a8dcb54dcdbe37bd435d5f
Rohit Ramesh <rkr@berkeley.edu>
2019-07-07 13:34:36 -0700

    Fleshed out BindingUpdate interface.

--- src/Ivy/Scratchpad.hs
+++ src/Ivy/Scratchpad.hs
@@ -30,72 +30,121 @@ data TV t v where
   --   values repeatedly.
   V :: {getV :: !v} -> TV t v
 
+class (Traversable t, Eq1 t, Hashable1 t) => Unifiable t  where
 
-class (Traversable t, Eq1 t, Hashable1 t)
-      => Unifiable t  where
-
+  -- | I like the model where we just create constraints that cover
+  --   various classes, and have one big'ol datatype to implement all
+  --   of the potential errors.
   type UnificationErr t e :: Constraint
 
-  unifyTerm :: (UnificationErr t e) => t v -> t v -> Either e (t (Either (v,v) v))
+  -- | Attempt to unify the following terms. A failure should return
+  --   `Left e` and a success should return a `Right _` where
+  --   any `Left (v,v)` will be further unified.
+  --
+  --   TODO :: Yeah, I know that latter type is getting to be a bit much
+  --          but it'll do for now. It's not clear that a new datatype
+  --          for this one use is actually useful.
+  unifyTerm :: (UnificationErr t e)
+            => t v -> t v
+            -> Either e (t (Either (v,v) v))
 
 
--- | This class presents a map of variables to (recursive) terms in a
---   continuation passing style.
+-- | This class is a somewhat modified version of
+--   `Control.Unification.BindingMonad` from `unification-fd`. It
+--   still performs structural unification, but with a few key differences:
 --
---   Yeah, CPS style is unusual and a bit more complicated than it
---   probably needs to be, but it allows for much finer control of
---   what actions happen when, and what they do.
+--     1) It doesn't choke on cyclic terms.
+--     2) Provides a rollback mechanism that can still return information
+--        from reverted branches of execution.
+--     3) Triggers hooks when terms are unified.
 --
---   Really, the fundamental goal of this interface is to allow backtracking
---   over sets of changes, while preserving useful information from them and
---   preventing leaks of references from domain to domain.
+--   All of these properties are desired if we want to use unification
+--   as a significant part of a synthesis process, or as an element in the
+--   analysis of inherently cyclic term graphs.
 --
---   We allow `m` to be of any kind because there's no real requirement it
+--   Specifically, the rollback mechanism is meant to interact seamlessly
+--   with query modes for modern SMT solvers. This mode allows you to
+--   push and pop sets of constraints from a stack while preserving
+--   any relevant learned clauses.
+--
+--   Note: We allow `m` to be of any kind because there's no real requirement it
 --   be a monad or something. This interface explicitly chooses to not require
---   a way to extract m, since it could either be a monad of some sort or
---   just a classic container.
-class (Unifiable t) => UnificationMap (m :: k) t | m -> t where
+--   a way to extract m, since it could be a monad, a pure container, or
+--   something else entirely.
+--
+--   Note: The operations `unify`, `equals`, `equiv`, and `subsume` /can/ all be
+--   implemented in terms of the `*Var` operations, but in a number of
+--   instances we will be able to get radical speedups by restricting `t`
+--   and specializing those ops.
+--
+--   FIXME :: Having `Hook` as an associated type family in this class
+--           renders the interface for hooks basically useless.
+--           In a better world, we'd either:
+--
+--             1) Provide functions in this class that allow you to construct
+--                a hook. (Good)
+--
+--             2) Standardize the structure of a hook so that it's just
+--                a function of some sort. (Best)
+class (Unifiable t, Semigroup (Hook m))
+      => BindUpdate (m :: k) t | m -> t where
 
   -- | This is an update that tells us how to modify a unification map.
+  --
+  --   The intention is that an implementer doesn't export `res` to
+  --   a user.
   type Update m = (res :: Type -> Type) | res -> m
 
   -- | This is a variable that has a unifiable term associated with it.
   --   generally defined by `m` in one way other.
+  --
+  --   The intention here is that `Var m` type is hidden from users
+  --   such that they can only use the functions in this typeclass to
+  --   work with them.
   type Var m :: Type
-
-  -- | If we want to manage hooks in some way, we can give them keys so
-  --   we can recover or manipulate them.
-  type HookKey m :: Type
 
   -- | The type of hooks for this type of unification map.
   type Hook m :: Type
 
-  -- | Pass in a continuation that uses a new variable to get a map update
-  --   and get the corresponding update.
-  --
-  --   We use a first order type here in order to limit the scope of
-  --   variable being created. As it is, one shouldn't be able to
-  --   extract a key from the continuation
+  -- | Create a new free (unbound) variable.
   freeVar :: Update m (Var m)
-
-  -- | Sets a variable to some term, in this case we use the
-  --   convenient little existential type wrapper for vars to
-  --   make them mildly easier to work with.
-  --
-  --   This should probably be set up so that the only way to
-  --   extract one of these vars is in a new context that
-  setVar :: t (Var m) -> Var m -> Update m ()
-
-  -- | Asserts that the first variable is <= the second.
-  subsumes :: (Var m) -> (Var m) -> Update m Bool
-
-  -- | Tells us whether two terms have been unified. Does not change
-  --   the state of the update, just return information.
-  equals :: (Var m) -> (Var m) -> Update m Bool
 
   -- | Get the single layer term for some variable or `Nothing` if
   --   the var is unbound.
-  getTerm  :: Var m  -> Update m (Maybe (t (Var m)))
+  lookupVar  :: Var m -> Update m (Maybe (t (Var m)))
+
+  -- | Binds a variable to some term, unifying it with any existing
+  --   term for that variable if needed.
+  bindVar :: Var m -> t (Var m) -> Update m ()
+
+  -- | Unifies two variables and their associated terms. If the
+  --   unification fails the transaction should be rolled back
+  --   and some error returned through whatever error mechanism
+  --   `Update m` uses.
+  unify :: Var m -> Var m -> Update m (Var m)
+
+  -- | Asserts that the first variable is <= the second.
+  subsumes :: Var m -> Var m -> Update m Bool
+
+  -- | Tells us whether two terms have been unified. Does not change
+  --   the state of the update, just return information.
+  equals :: Var m -> Var m -> Update m Bool
+
+  -- | Tells us whether the input terms are equivalent modulo renaming of
+  --   free variables. If they are, returns a set of unifications that
+  --   need to occur for both terms to be truly equivalent.
+  equiv :: Var m -> Var m -> Update m Bool
+
+  -- | Adds a single hook to the unification map. Semantically this should
+  --   use the semigroup instance of `Hook m` to merge the new hook with
+  --   all previous hooks.
+  --
+  --   A hook, whatever the exact form it takes, should be triggered whenever
+  --   there is a change to
+  addHook :: Hook m -> Var m -> Update m ()
+
+  -- | Returns the hook for some given variable.
+  getHook :: Var m -> Update m (Hook m)
 
   -- | Given two updates, this produces a parallel merge of those two updates.
   --   really, it's the implementation of this that determines the degree of
@@ -107,25 +156,6 @@ class (Unifiable t) => UnificationMap (m :: k) t | m -> t where
            -> Update m b
            -> Update m (a,b)
 
-  -- | Adds a single hook to the unification map, by default
-  addHook :: HookKey m -> Hook m -> Var m -> Update m ()
-  addHook k h v = addHooks [(k,[h])] v
-
-  -- | Add hooks that are run when the term for some variable is changed.
-  --   Yeah, this is a bit weird, since it's how we basically create
-  --   propagator update hooks.
-  addHooks :: [(HookKey m, [Hook m])]
-          -> Var m
-          -> Update m ()
-
-  -- | Retrieves all the hooks for some given key and var.
-  getHooks :: HookKey m -> Var m -> Update m [Hook m]
-
-  -- | Gets every hook for some given key.
-
-  -- | Removes all the hooks for a variable under some key.
-  removeHooks :: HookKey m -> Var m -> Update m [Hook m]
-
   -- | Try an update, if the action should be rolled back (returns a `Left f`)
   --   then do so, and run the recovery function.
   --
@@ -133,35 +163,12 @@ class (Unifiable t) => UnificationMap (m :: k) t | m -> t where
   --   some knowledge that has been gained from the rolled back computation.
   --   Say if you're doing CDCL `f` would be the newly learned conflict clause.
   --
-  --   The semantics if you do hit an error within the initial transaction
-  --   aren't super well defined
+  --   An uncaught error in the initial action should rollback the state to
+  --   the start of the action and then allow the error to continue propagating
+  --   upward, without running the recovery action.
   attempt :: Update m (Either f s) -> (f -> Update m b) -> Update m (Either b s)
 
   -- | Combine one update which happens after another into a single
   --   (hopefully somehow compressed or minimized) update. This is probably
-  --   just a bind of some sort.
+  --   just a bind of some sort if `Update m` is a monad.
   sequence :: Update m a -> (a -> Update m b) -> Update m b
-
-
-{-
--- | If you have an expression, generate a new variable
-newVar :: (Monad (Update m v t), UnificationMap e m t) => TV t v -> Update m v t v
-newVar = __TODO
-
-unify :: forall e m t v. (UnificationMap e m t, Ref v, Monad (Update m v t), Semigroup e)
-      => v -> v -> Update m v t (Either e v)
-unify a b = do
-  eta <- getTerm @e @m a
-  etb <- getTerm @e @m b
-  case (eta,etb) of
-    (Left e, Left e') -> pure . Left $ e <> e'
-    (Left e, _) -> pure . Left $ e
-    (_, Left e) -> pure . Left $ e
-    (ta, tb) -> __TODO
-    -- Try unifying the term, w/in a wrapper that assumes a and b are merged.
-    -- if you get an error out then return that.
-    -- otherwise, traverse through the term replacing pairs and terms
-    -- with `Either e a`
-    --   if there are 1 or more errors, `<>` them and return that
-    --   if not then bind the result term to a and return a
--}
